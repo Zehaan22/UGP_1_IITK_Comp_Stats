ea7a7524116b7081f16d1909fe130b722a6dcde1
Zehaan <zehaan22@iitk.ac.in>
2024-08-16 12:32:44 +0530

    another attempt at HMC with MH

--- HMCwithMH.R
+++ HMCwithMH.R
@@ -1,3 +1,4 @@
+library(pracma)
 N <- 1e4
 k <- 50
 iid.samples <- rt(n = N, df = k)
@@ -8,13 +9,16 @@ t_mh <- function(N = 1e3, k, h){
   
   out.vect <- numeric(length = N)
   acc.prob <- 0
-  out.vect[1] <- 2
+  out.vect[1] <- 5
   
   for(t in 2:N){
     prop.curr <- rnorm(1,out.vect[t-1],sqrt(h))
     y.curr <- prop.curr^3
     
-    alpha <- (dt(prop.curr, k)*sqrt(abs(out.vect[t-1])))/(dt(out.vect[t-1],k)*sqrt(abs(y.curr))) # Since q is normal hence q(qbrt(x)|z)/q(qbrt(z)|x) = 1    
+    alpha.num <- (dnorm(nthroot(out.vect[t-1],3),y.curr,sqrt(h))) * (dt(y.curr, k)) * (nthroot(out.vect[t-1],3)^(-2))
+    alpha.den <- (dnorm(prop.curr,out.vect[t-1],sqrt(h))) * (dt(out.vect[t-1], k)) * (prop.curr^(-2))
+    
+    alpha <- alpha.num / alpha.den
     
     alpha <- min(1,alpha)
     
@@ -32,7 +36,7 @@ t_mh <- function(N = 1e3, k, h){
   return(out.vect)
 }
 
-chain <- t_mh(N,k,20)
+chain <- t_mh(N,k,10)
 plot(density(iid.samples))
 lines(density(chain), col = "red")
 plot(density(chain))
