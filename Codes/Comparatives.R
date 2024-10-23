bf72295f1e0123a23ea55b6377223b8f15ba0af4
Zehaan <zehaan22@iitk.ac.in>
2024-10-23 11:19:08 +0530

    Added some visual and numerical tests for NUTS

--- Codes/Comparatives.R
+++ Codes/Comparatives.R
@@ -1,45 +1,79 @@
 source("Toy_NUTS.R")
-
+library(mcmcse)
 
 # For Theoretical HMC
+print("Theoretical HMC:")
 chain1 <- normalHMC(s = .1)
+ess(chain1)
 chain2 <- normalHMC(s = 1)
+ess(chain2)
 chain3 <- normalHMC(s = 5)
+ess(chain3)
+print("###########################")
 
 # Comparing the chains through plots
-plot(density(chain1), col = "blue")
+par(mfrow = c(1,1))
+plot(density(chain1), col = "blue", main = "Theoretical HMC")
 lines(density(chain2), col = "green")
 lines(density(chain3), col = "red")
 
-plot.ts(chain1, col = "blue")
-plot.ts(chain2, col = "green")
-plot.ts(chain3, col = "red")
+par(mfrow = c(3,1))
+plot.ts(chain1, col = "blue", main = "Theoretical HMC | s = 0.1")
+plot.ts(chain2, col = "green", main = "Theoretical HMC | s = 1")
+plot.ts(chain3, col = "red", main = "Theoretical HMC | s = 5")
+
+acf(chain1, main = "Theoretical HMC | s = 0.1")
+acf(chain2, main = "Theoretical HMC | s = 1")
+acf(chain3, main = "Theoretical HMC | s = 5")
 
 # For LeapFrog
 #Keeping L * epsilon = 1
+print("Leap Frog:")
 chain1 <- normalLF_HMC(L = 10, eps = .1)
+ess(chain1)
 chain2 <- normalLF_HMC(L = 1, eps = 1)
+ess(chain2)
 chain3 <- normalLF_HMC(L = 100, eps = .01)
+ess(chain3)
+print("###########################")
 
 # Comparing the chains through plots
-plot(density(chain1), col = "blue")
+par(mfrow = c(1,1))
+plot(density(chain1), col = "blue", main = "Leap Frog")
 lines(density(chain2), col = "green")
 lines(density(chain3), col = "red")
 
-plot.ts(chain1, col = "blue")
-plot.ts(chain2, col = "green")
-plot.ts(chain3, col = "red")
+par(mfrow = c(3,1))
+plot.ts(chain1, col = "blue", main = "Leap Frog | L = 10 e = 0.1")
+plot.ts(chain2, col = "green", main = "Leap Frog | L = 1, e = 1")
+plot.ts(chain3, col = "red", main = "Leap Frog | L = 100, e = .01")
 
-#Trying out different values of epsilon
+acf(chain1, main = "Leap Frog | L = 10 e = 0.1")
+acf(chain2, main = "Leap Frog | L = 1, e = 1")
+acf(chain3, main = "Leap Frog | L = 100, e = .01")
+
+
+#Trying out different values of epsilon in NUTS
+print("NUTS Algorithm:")
 chain1 <- nut_sampler(e = .1)
+ess(chain1)
 chain2 <- nut_sampler(e = 1)
+ess(chain2)
 chain3 <- nut_sampler(e = .01)
+ess(chain3)
+print("###########################")
 
 # Comparing the chains through plots
-plot(density(chain1), col = "blue")
+par(mfrow = c(1,1))
+plot(density(chain1), col = "blue", main = "NUTS Algorithm")
 lines(density(chain2), col = "green")
 lines(density(chain3), col = "red")
 
-plot.ts(chain1, col = "blue")
-plot.ts(chain2, col = "green")
-plot.ts(chain3, col = "red")
+par(mfrow = c(3,1))
+plot.ts(chain1, col = "blue", main = "NUTS Algorithm | e = 0.1")
+plot.ts(chain2, col = "green", main = "NUTS Algorithm | e = 1")
+plot.ts(chain3, col = "red", main = "NUTS Algorithm | e = 0.01")
+
+acf(chain1, main = "NUTS Algorithm | e = 0.1")
+acf(chain2, main = "NUTS Algorithm | e = 1")
+acf(chain3, main = "NUTS Algorithm | e = 0.01")
--- Codes/Toy_NUTS.R
+++ Codes/Toy_NUTS.R
@@ -70,55 +70,57 @@ leapFrog <- function(p,q,e){
   p_ <- p - (e/2)*q
   q_ <- q + e*p_
   p_ <- p_ - (e/2)*q_
-  return(c(p_,q_))
+  return(list(p_,q_))
 }
 
 travelRoad <- function(p,q,u,v,j,e, del_max = 1e-6){
   if (j == 0){ #Base Case
     rets <- leapFrog(p,q,v*e)
-    p_ <- rets[1]
-    q_ <- rets[2]
+    p_ <- rets[[1]]
+    q_ <- rets[[2]]
     if(u <= exp(-(q^2+p^2)/2)){
-      set_C <- rets
+      set_C <- c(rets[[1]],rets[[2]])
     }
     else{
       set_C <- NaN
     }
     s_ <- -(q_^2)/2 - (p^2)/2 > log(u) - del_max
-    return(c(p_,q_,p_,q_,set_C,s_))
+    return(list(p_,q_,p_,q_,set_C,s_))
   }
   else{ # Recursive Case
     rets <- travelRoad(p,q,u,v,j-1,e)
-    p_min <- rets[1]
-    q_min <- rets[2]
-    p_pls <- rets[3]
-    q_pls <- rets[4]
-    Set_C_1 <- rets[5]
-    s_1 <- rets[6]
+    p_min <- rets[[1]]
+    q_min <- rets[[2]]
+    p_pls <- rets[[3]]
+    q_pls <- rets[[4]]
+    Set_C_1 <- rets[[5]]
+    s_1 <- rets[[6]]
     if (v == -1){
       rets_min <- travelRoad(p_min,q_min,u,v,j-1,e)
-      p_min <- rets_min[1]
-      q_min <- rets_min[2]
-      Set_C_2 <- rets_min[5]
-      s_2 <- rets_min[6]
+      p_min <- rets_min[[1]]
+      q_min <- rets_min[[2]]
+      Set_C_2 <- rets_min[[5]]
+      s_2 <- rets_min[[6]]
     }
     else{
-      rets_min <- travelRoad(rets[3],rets[4],u,v,j-1,e)
-      p_pls <- rets_min[3]
-      q_pls <- rets_min[4]
-      Set_C_2 <- rets_min[5]
-      s_2 <- rets_min[6]
+      rets_min <- travelRoad(p_pls,q_pls,u,v,j-1,e)
+      p_pls <- rets_min[[3]]
+      q_pls <- rets_min[[4]]
+      Set_C_2 <- rets_min[[5]]
+      s_2 <- rets_min[[6]]
     }
     s_ <- s_1*s_2*((q_pls-q_min)*p_min>=0)*((q_pls-q_min)*p_pls>=0)
     Set_C_1 <- rbind(Set_C_1, Set_C_2)
-    return(c(p_min,q_min,p_pls,q_pls,Set_C_1,s_))
+    return(list(p_min,q_min,p_pls,q_pls,Set_C_1,s_))
   }
 }
 
 nut_sampler <- function(e,n=1e4){
   q = numeric(n)
+  jumps = numeric(n)
   q_0 = 0
   for(i in 1:n){
+    
     p <- rnorm(1)
     u <- runif(1,0,exp(-((p^2)+(q_0^2))/2))
     q_min <- q_0
@@ -133,17 +135,17 @@ nut_sampler <- function(e,n=1e4){
       v = sample(c(-1,1),1)
       if(v == -1){
         rets <- travelRoad(p_min,q_min,u,v,j,e)
-        p_min <- rets[1]
-        q_min <- rets[2]
-        Set_C_1 <- rets[5]
-        s_1 <- rets[6]
+        p_min <- rets[[1]]
+        q_min <- rets[[2]]
+        Set_C_1 <- rets[[5]]
+        s_1 <- rets[[6]]
       }
       else{
         rets <- travelRoad(p_pls,q_pls,u,v,j,e)
-        p_pls <- rets[3]
-        q_pls <- rets[4]
-        Set_C_1 <- rets[5]
-        s_1 <- rets[6]
+        p_pls <- rets[[3]]
+        q_pls <- rets[[4]]
+        Set_C_1 <- rets[[5]]
+        s_1 <- rets[[6]]
       }
       if(s_1 == 1){
         Set_C <- rbind(Set_C,Set_C_1)
@@ -155,8 +157,9 @@ nut_sampler <- function(e,n=1e4){
     q_0 <- Set_C[n_0,]
     q_0 <- q_0[2]
     q[i] <- q_0
+    jumps[i] <- j
   }
-  
+  print(summary(jumps))
   return(q)
 }
 
